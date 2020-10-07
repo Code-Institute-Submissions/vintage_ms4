e5e7eb41dd77fd24881eabffa9a2ce3f1c742cb1
sneachda <savela.aleks@gmail.com>
2020-10-07 16:50:32 +0100

    added products app and media folder which includes all img for the products. Created fixtures folder and added products categories and designer json files with all the data

---
+++ products/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Designer(models.Model):
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    designer = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=2000)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image1 = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
+    image4 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
