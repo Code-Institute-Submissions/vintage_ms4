a35db5397473c4c7e9b73cce085af23725ad6e9e
sneachda <savela.aleks@gmail.com>
2020-10-12 16:15:27 +0100

    added no-image image, created a detailed view of products page, updated products.html with links

--- products/views.py
+++ products/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ A view displaying all the products, also includes sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET('q')
+            if not query:
+                messages.error(request, "You didn't eneter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(designer__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def single_product(request, product_id):
+    """ A view displaying details of a single product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/single_product.html', context)
