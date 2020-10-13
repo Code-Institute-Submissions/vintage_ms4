7213310e840b73043becf06d4e0b471c541def02
sneachda <savela.aleks@gmail.com>
2020-10-13 16:47:41 +0100

    implemented search by categories and designers option

--- products/views.py
+++ products/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Designer
 
 
 def all_products(request):
     """ A view displaying all the products, also includes sorting and search queries """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
+    designers = Designer.objects.all()
     query = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'designer' in request.GET:
+            designers = request.GET['designer'].split(',')
+            products = [product for product in products if product.designer == designers[0]]
+            designers = Designer.objects.filter(name__in=designers)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,7 +34,11 @@ def all_products(request):
 
     context = {
         'products': products,
+        'categories': categories,
+        'designers': designers,
         'search_term': query,
+        'current_categories': categories,
+
 
     }
 
