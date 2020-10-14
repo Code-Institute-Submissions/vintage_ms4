7c086e672a6c971019c6a67430b639973221f0dd
sneachda <savela.aleks@gmail.com>
2020-10-14 18:28:38 +0100

    remove from cart link functionality created

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib import messages
 
 from products.models import Product
@@ -28,3 +28,19 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+
+def remove_from_cart(request, item_id):
+    """Allow user to remove item from the cart"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('cart', {})
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cat')
+
+        request.session['cart'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
