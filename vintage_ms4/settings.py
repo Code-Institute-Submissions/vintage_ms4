96bfc2611d56acdf96bf6c55361cd4368709a891
sneachda <savela.aleks@gmail.com>
2020-10-15 19:00:34 +0100

    installed crispy forms. Created checkout models views and basic template

--- vintage_ms4/settings.py
+++ vintage_ms4/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'about',
     'checkout',
 
+    # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'vintage_ms4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -79,6 +84,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
