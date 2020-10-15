471615040e11b5b52e1e7e8073f34f55288d637a
sneachda <savela.aleks@gmail.com>
2020-10-15 20:01:45 +0100

    updated the checkout html, added stripe to project settings and created stripe js and css

--- checkout/forms.py
+++ checkout/forms.py
@@ -23,6 +23,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
+            'postcode': 'Postcode',
         }
 
         self.fields['first_name'].widget.attrs['autofocus'] = True
--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
-
-# Create your views here.
 
 
 def checkout(request):
     cart = request.session.get('cart', {})
     if not cart:
-        messages.error(request, 'Your bag is empty')
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    order_form = OrderForm()
+    order_form = OrderForm
     template = 'checkout/checkout.html'
-    context: {
-        'order_form': order_form
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HRghCKvKuGGOibTXxmZxLxFIOztxDJrzmZXrtONcjfMBiy8GfvXyrdPrvS75SBH3O71GhY0IPl4BUHiAgS2A3UA00fu0XFm5J',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
