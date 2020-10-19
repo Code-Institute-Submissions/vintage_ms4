cf72a0d3e887a5c3b3f076b54d058db7b7e62c2e
sneachda <savela.aleks@gmail.com>
2020-10-19 09:55:10 +0100

    added edit product functionality

--- products/views.py
+++ products/views.py
@@ -99,3 +99,31 @@ def add_product(request):
 
     return render(request, template, context)
 
+
+def edit_antiquity(request, product_id):
+    """ Edit an item """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only admin can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated an item!')
+            return redirect(reverse('single_product', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update an item. Please ensure that the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
