453e56e29d301cef244d4f95d7ca365baa4c018c
sneachda <savela.aleks@gmail.com>
2020-10-26 08:26:24 +0000

    EmailJs - subscription service added

---
+++ static/js/emailjs.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('subscribe-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8jquzfe';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
