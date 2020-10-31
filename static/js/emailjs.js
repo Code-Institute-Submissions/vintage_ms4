c44a1aa080118b639711af2729aca09b463dc8e5
sneachda <savela.aleks@gmail.com>
2020-10-31 17:29:11 +0000

    emailjs functionality updated

--- static/js/emailjs.js
+++ static/js/emailjs.js
@@ -1,15 +1,15 @@
 // https://stackoverflow.com/questions/63265789/how-do-i-send-my-form-information-on-submit-to-my-email-with-emailjs
+const btn = document.getElementById('sub-button');
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "vintage", {
         "from_email": contactForm.email.value,
-    }).then(
-        function() {
-             alert("Email sent successfully!");
+    }).then(() => {
+        btn.value = 'Subscribe';
+        alert('Success!');
+    }, (err) => {
+        btn.value = 'Subscribe';
+        alert(JSON.stringify(err));
+    });
 
-
-        },
-         function(error) {
-            alert("FAILED!"+error);
-        });
 }
